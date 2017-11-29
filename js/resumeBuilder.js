d90743cc9034384c2cbbde9e7d16bd00441855b4
Kyle <ksekstrand@gmail.com>
2017-11-28 23:04:04 -0600

    Added base object to iterate thru

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,95 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ /*
+ Begin JS objects area
+ */
+
+const bio = {
+  name : "Kyle Ekstrand",
+  role : "Guy who codes things",
+  contacts : {
+        mobile: '(111)867-5309',
+        email: 'ksekstrand@gmail.com',
+        github: 'ksekstrand',
+        twitter: '',
+        location: 'USA'
+      },
+  welcomeMessage: "Hello Friends, this is my welcome message!",
+  skills: {
+    skill1: 'petting dogs',
+    skill2: 'eating peanut butter',
+    skill3: 'bumping into coffee tables'
+  },
+  biopic: "http://idonthaveabiopic.com",
+  display: ()=>{
+
+  }
+}
+
+const education = {
+  schools: {
+     name: "Univesity of Alaska Anchorage",
+     location: "Anchorage, AK",
+     degree: "Management Information Systems",
+     majors: "Economics, Management Information Systems",
+     dates: "2008-2015",
+     url: "https://www.uaa.alaska.edu"
+   },
+  onlineCourses: {
+       title: "Udacity Front End Developer",
+       school: "Udacity",
+       dates: "2016-2017",
+       url: "https://udacity.com"
+     }
+  display: () => {
+
+  }
+}
+
+const jobs: {
+  jobList:{
+    job1:{
+      employer: "USAA"
+      title: "Software Developer and Integrator II",
+      location: "San Antonio, TX",
+      dates: "2015-Current",
+      description: "I write code on the mainframe (primaily C++ / Q++ code running on CICS transactions on the z/os platform), java tier (RESTful JAX-RS web services and Wicket), and the UI occassionally."
+    },
+    job2{
+      employer: "Northern Air Cargo",
+      title: "Systems Analyst II",
+      location: "Anchorage, AK",
+      dates: "2014-2015",
+      description: "Application administrator and all-around IT problem solver for a variety of functions, including cargo systems, SAP, and inventory management UDA creation",
+    }
+  }
+}
+display: () => {
+
+}
+
+const projects {
+  projectList{function taking no parameters
+    project1:{
+      title: "",
+      dates: "",
+      description: "",
+      images: ""
+    },
+    project2:{
+      title: "",
+      dates: "",
+      description: "",
+      images: ""  
+    }
+  }
+  display: () =>  {
+
+  }
+}
+
+/*
+End JS objects area
+*/
